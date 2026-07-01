0b019fff27c6a2afdbdffbfdc66516110346ceb3
oportunitas <taibizzat165@gmail.com>
2026-07-01 16:40:29 +0700

    ✨ branches

---
+++ branches/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    let condition = false;
+    let number = if condition { 5 } else { "six" };
+
+    println!("the value of number is : {number}");
+}
+
+// fn main() {
+//     let number = 0;
+//     if number != 0 {
+//         println!("number was not zero");
+//     } else {
+//         println!("number was zero");
+//     }
+// }
+
+// fn main() {
+//     let number = 6;
+//     if number < 5 {
+//         println!("condition was true");
+//     } else {
+//         println!("condition was false");
+//     }
+// }
